ed9d83e44b2c650c8341e47d92bffde3a833e476
aadversteeg <aad@versteeg-its.nl>
2022-12-24 20:55:09 +0100

    Added unit tests

--- src/Ave.Extensions.Functional.FluentAssertions/ResultExtensions.cs
+++ src/Ave.Extensions.Functional.FluentAssertions/ResultExtensions.cs
@@ -2,6 +2,6 @@
 {
 	public static class ResultExtensions
 	{
-		public static ResultAssertions<T,E> Should<T>(this Result<T,E> instance) => new ResultAssertions<T,E>(instance);
+		public static ResultAssertions<T,E> Should<T,E>(this Result<T,E> instance) => new ResultAssertions<T,E>(instance);
 	}
 }
